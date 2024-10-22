832cf0db25808ab4f70fec3efa1e1ff1c9084da2
Toretto <145860512+Torettosaviour17@users.noreply.github.com>
2024-10-22 14:45:48 +0100

    I just finished working on FirstNav

--- src/screens/homescreen.jsx
+++ src/screens/homescreen.jsx
@@ -5,10 +5,12 @@ import Footer from '../components/layout/footer'
 import OurVoice from '../components/home-component/our_voice'
 import OurAim from '../components/home-component/our-aim'
 import SignIn from '../components/home-component/sign-in'
+import FirstNav from '../components/home-component/firstNav'
 
 export default function homescreen() {
   return (
     <div>
+      <FirstNav />
       <Navbar />
       <Hero />
       <OurVoice />
